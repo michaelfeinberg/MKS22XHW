f8ab6a7e0e84352668ed68a102a3a85ff1756577
michaelfeinberg <mfeinberg32999@gmail.com>
2016-02-12 12:17:30 -0500

    Completed for open and closed tours, as well as rectangular boards

--- 03KnightTour/KnightBoard.java
+++ 03KnightTour/KnightBoard.java
@@ -6,6 +6,10 @@ public class KnightBoard{
 	board = new int[n][n];
 	x = 0;
     }
+    public KnightBoard(int row,int col){
+	board = new int[row][col];
+	x = 0;
+    }
     public boolean ClosedSolve(){
 	return ClosedSolveHelp(1,0,0);
     }
@@ -16,17 +20,17 @@ public class KnightBoard{
 	//}
 	x++;
 	//System.out.println(x);
-        if(row>=board.length||row<0||col>=board.length||col<0){
+        if(row>=board.length||row<0||col>=board[0].length||col<0){
 	    return false;
 	}
 	for(int i = 0;i<board.length;i++){
-	    for(int j = 0;j<board.length;j++){
+	    for(int j = 0;j<board[0].length;j++){
 		if(board[i][j]>=count){
 		    board[i][j]=0;
 		}
 	    }
 	}
-	if(count == board.length*board.length+1){
+	if(count == board.length*board[0].length+1){
 	    if(board[row][col]== 1){
 		return true;
 	    }else{
@@ -46,6 +50,37 @@ public class KnightBoard{
 		    ClosedSolveHelp(count+1,row-2,col-1));
 	}
     }
+    public boolean solve(){
+	return solveHelp(1,0,0);
+    }
+    public boolean solveHelp(int count,int row,int col){
+	x++;
+	if(row>=board.length||row<0||col>=board[0].length||col<0){
+            return false;
+        }
+        for(int i = 0;i<board.length;i++){
+            for(int j = 0;j<board[0].length;j++){
+                if(board[i][j]>=count){
+                    board[i][j]=0;
+                }
+            }
+        }
+        if(count == board.length*board[0].length+1){
+	    return true;
+	}else if(board[row][col]>0){
+	    return false;
+	}else{
+	    board[row][col]=count;
+	    return (solveHelp(count+1,row+2,col+1)||
+		    solveHelp(count+1,row+2,col-1)||
+		    solveHelp(count+1,row+1,col+2)||
+		    solveHelp(count+1,row+1,col-2)||
+		    solveHelp(count+1,row-1,col+2)||
+		    solveHelp(count+1,row-1,col-2)||
+		    solveHelp(count+1,row-2,col+1)||
+		    solveHelp(count+1,row-2,col-1));
+	}
+    }
     public void printSolution(){
 	String ans = "";
 	for(int i = 0;i < board.length; i++){
@@ -55,7 +90,7 @@ public class KnightBoard{
     }
     public String printRow(int n){
 	String ans = "";
-	for(int j = 0;j<board.length;j++){
+	for(int j = 0;j<board[0].length;j++){
 	    if(board[n][j]<10){
 		ans += board[n][j] + "  ";
 	    }else{
@@ -65,9 +100,15 @@ public class KnightBoard{
 	return ans;
     }
     public static void main(String[]args){
-	KnightBoard K = new KnightBoard(Integer.parseInt(args[0]));
+	KnightBoard K;
+	if(args.length > 1){
+	    K = new KnightBoard(Integer.parseInt(args[0]),
+				Integer.parseInt(args[1]));
+	}else{
+	    K = new KnightBoard(Integer.parseInt(args[0]));
+	}
 	System.out.println(K.solve());
-	//System.out.println(K.x + " recursive calls");
+	System.out.println(K.x + " recursive calls");
 	K.printSolution();
     }
 }
